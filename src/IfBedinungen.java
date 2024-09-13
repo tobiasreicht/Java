d196ae708ec7031e27301a6c7445a2b2d8ebcb7f
tobiasreicht <tobias.reicht@student.htldornbirn.at>
2024-09-13 13:05:06 +0200

    ifbedinungen

---
+++ src/IfBedinungen.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class IfBedinungen {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        int randomnumber = random.nextInt(100);
+
+        if (randomnumber < 20){
+            System.out.println(randomnumber + "mini");
+        } else if (randomnumber > 20) {
+            System.out.println(randomnumber + "medium");
+        } else {
+            System.out.println(randomnumber + "Large");
+        }
+    }
+}
