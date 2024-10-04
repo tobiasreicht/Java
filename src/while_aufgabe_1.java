84cba5c846dd3244a4994c49f2ef7bc792e5bd9c
tobiasreicht <tobias.reicht@student.htldornbirn.at>
2024-10-04 11:15:10 +0200

    while_aufgabe_1 done

--- src/while_aufgabe_1.java
+++ src/while_aufgabe_1.java
@@ -2,19 +2,20 @@ import java.util.Random;
 
 public class while_aufgabe_1 {
     public static void main(String[] args) {
-        long result = 0;
-        Random random =  new Random();
-        int randomNumber = random.nextInt(31-10)+10;
 
-        if (randomNumber == 15 || randomNumber == 25) {
-            break;
+        long result = 0;
+        Random random = new Random();
+        while (true) {
+            int randomNumber = random.nextInt(21) + 10;
+            System.out.println(randomNumber);
+
+            if (randomNumber == 15 || randomNumber == 25) {
+                break;
+            }
+
+            result += randomNumber;
         }
 
-        System.out.println(randomNumber);
-
-        result += randomNumber;
-        System.out.println(result);
+        System.out.println("Summe der Zufallszahlen: " + result);
     }
-
-
 }
